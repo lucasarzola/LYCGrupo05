57be63c7a36f613d25ca9d84a71895ea67b15b1e
Lucas Arzola <42591339+lucasarzola@users.noreply.github.com>
2020-06-07 21:46:00 -0300

    Correccion del GeneraExe.bat

--- Grupo5/SegundaEntrega/GeneraExe.bat
+++ Grupo5/SegundaEntrega/GeneraExe.bat
@@ -2,14 +2,13 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Segunda.exe
 pause
 pause
-Primera.exe prueba.txt
+Segunda.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
-del Primera.exe
 
 pause
