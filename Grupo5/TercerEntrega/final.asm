b91434e3b4213d4bf0a35599640d6c3a68edcd85
Luciano Artale <34487376+LucianoArtale@users.noreply.github.com>
2020-06-28 19:11:57 -0300

    Add files via upload

--- Grupo5/TercerEntrega/final.asm
+++ Grupo5/TercerEntrega/final.asm
@@ -2,13 +2,30 @@ include macros2.asm
 include number.asm
 
 .MODEL LARGE
-.STACK 200h
 .386
-.387
+.STACK 200h
+
 .DATA
 
-	MAXTEXTSIZE equ 50
-	FLD 1
-	FLD 2
-	FADD
-	FSTP a
+a   dd  ?
+b   dd  ?
+result  dd  ?
+R   dd  ?
+_100m   dd  100000.0
+
+.CODE
+    MOV AX,@DATA
+    MOV DS,AX
+    FINIT
+FFREE
+FLD _100m
+FSTP    a
+DisplayFloat a,2
+FINAL:
+    mov ah, 1
+    int 21h
+    MOV AX, 4c00h
+    INT 21h
+END
+
+;FIN DEL PROGRAMA DE USUARIO
