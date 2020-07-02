37310e528a50c97f5ab9d43dc2e21157b519da33
lucasarzola <lucas.arzola@gmail.com>
2020-07-02 13:49:22 -0300

    Modificacion GeneraExe.bat

--- Grupo5/TercerEntrega/GeneraExe.bat
+++ Grupo5/TercerEntrega/GeneraExe.bat
@@ -2,10 +2,10 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Segunda.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Tercera.exe
 pause
 pause
-Segunda.exe prueba2.txt
+Tercera.exe prueba2.txt
 del lex.yy.c
 del y.tab.c
 del y.output
