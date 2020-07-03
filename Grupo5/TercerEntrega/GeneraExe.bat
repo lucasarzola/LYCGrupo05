9e744a1e3223acca8b9847b96390eb0ffe6ca102
lucasarzola <lucas.arzola@gmail.com>
2020-07-03 00:12:31 -0300

    agregado de partes

--- Grupo5/TercerEntrega/GeneraExe.bat
+++ Grupo5/TercerEntrega/GeneraExe.bat
@@ -2,10 +2,9 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Tercera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Grupo5.exe
 pause
-pause
-Tercera.exe prueba2.txt
+Grupo5.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
